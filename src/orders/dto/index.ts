bdf7713c5b522949ae0624a9747307c35291a377
JorgeAVargasC <javargas1209@gmail.com>
2024-04-18 16:49:29 -0500

    [UPDATE] orders service

--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -1,4 +1,5 @@
 export * from './create-order.dto'
 export * from './enum/order.enum'
-export * from './status.dto'
+export * from './order-item-dto'
 export * from './order-pagination.dto'
+export * from './status.dto'
