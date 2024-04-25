f67676ab91bef03cf2ff01e6fe4297b24970d0d3
JorgeAVargasC <javargas1209@gmail.com>
2024-04-24 23:49:16 -0500

    [UPDATE] auth ms

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common'
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common'
 import { ClientProxy, RpcException } from '@nestjs/microservices'
 import { catchError } from 'rxjs'
 import { NATS_SERVICE } from 'src/config'
+import { LoginUserDto, RegisterUserDto } from './dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser() {
-    return this.client.send('auth.register.user', {}).pipe(
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
       catchError((error) => {
         throw new RpcException(error)
       })
@@ -17,8 +18,8 @@ export class AuthController {
   }
 
   @Post('login')
-  loginUser() {
-    return this.client.send('auth.login.user', {}).pipe(
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
       catchError((error) => {
         throw new RpcException(error)
       })
@@ -26,8 +27,8 @@ export class AuthController {
   }
 
   @Get('verify')
-  verifyUser() {
-    return this.client.send('auth.verify.user', {}).pipe(
+  verifyToken() {
+    return this.client.send('auth.verify.token', {}).pipe(
       catchError((error) => {
         throw new RpcException(error)
       })
